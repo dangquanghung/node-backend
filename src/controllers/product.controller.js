723272ff9f8dd9fcea0a1fc139339cae4f42a265
dangquanghung <hungvuive2000@gmail.com>
2024-08-27 13:35:49 +0700

    factury pattern, create product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,18 +1,18 @@
-'use strict'
+"use strict";
 
-const ProductService = require('../services/product.service')
-const { SuccessResponse } = require('../core/success.response')
+const ProductService = require("../services/product.service");
+const { SuccessResponse } = require("../core/success.response");
 
 class ProductController {
-    createProduct = async (req, res, next) => {
-        new SuccessResponse({
-            message: 'Create new Product success!',
-            metadata: await ProductService.createProduct(req.body.product_type, {
-                ...req.body,
-                product_shop: req.user.userId
-            })
-        }).send(res)
-    }
+  createProduct = async (req, res, next) => {
+    new SuccessResponse({
+      message: "Create new Product success!",
+      metadata: await ProductService.createProduct(req.body.product_type, {
+        ...req.body,
+        product_shop: req.user.userId,
+      }),
+    }).send(res);
+  };
 }
 
-module.exports = new ProductController()
+module.exports = new ProductController();
